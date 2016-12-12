eaf4ba7a4cb0b3c4273186faf665016a23366590
Sachindra Singh <sachindras@spc.int>
2016-12-13 11:24:12 +1300

    Sync Command Added.

---
+++ sync.sh
@@ -0,0 +1,3 @@
+docker cp logo.png dockerlizmappostgis_lizmap_1:/var/www/websig/lizmap/www/themes/default/css/img/
+docker cp logo.png dockerlizmappostgis_lizmap_1:/var/www/websig/lizmap/www/css/img/
+docker cp logo_footer.png dockerlizmappostgis_lizmap_1:/var/www/websig/lizmap/www/themes/default/css/img/
